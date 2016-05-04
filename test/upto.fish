4bc8d9f36a7b66abd97b4d3b50922fa5884fdd14
Markcial <markcial@gmail.com>
2016-05-04 22:55:37 +0200

    refactored so the tests now pass

--- test/upto.fish
+++ test/upto.fish
@@ -1,37 +1,38 @@
-function does_it_works -a folder -a param -a expected -a retcode
+function does_it_works -a folder -a param -a expected
   mock pwd 0 "echo '$folder'"
   mock cd 0 "echo \$args"
   set -l out (upto $param)
-  if test "$out" = "$expected" -a $retcode -eq $status
-    return 0
+  if test "$out" = "$expected"
+    return
   end
+
   return 1
 end
 
 test "$TESTNAME up just one level"
-    (does_it_works /root/spam/eggs spam /root/spam 0) $status -eq 0
+    (does_it_works /root/spam/eggs spam /root/spam) $status -eq 0
 end
 
 test "$TESTNAME up two levels"
-    (does_it_works /root/spam/eggs root /root 0) $status -eq 0
+    (does_it_works /root/spam/eggs root /root) $status -eq 0
 end
 
 test "$TESTNAME up three levels"
-    (does_it_works /root/one/two/three/four one /root/one 0) $status -eq 0
+    (does_it_works /root/one/two/three/four one /root/one) $status -eq 0
 end
 
 test "$TESTNAME up to the top"
-    (does_it_works /root/one/two/three/four root /root 0) $status -eq 0
+    (does_it_works /root/one/two/three/four root /root) $status -eq 0
 end
 
 test "$TESTNAME folder not found"
-    (does_it_works /root/one/two/three/four meh '' 1) $status -eq 0
+    (does_it_works /root/one/two/three/four meh '') $status -eq 0
 end
 
 test "$TESTNAME folders with weird chars"
-    (does_it_works '/root/boh/Ñç&(abc)/bacon' boh /root/boh 0) $status -eq 0
+    (does_it_works '/root/boh/Ñç&(abc)/bacon' boh /root/boh) $status -eq 0
 end
 
 test "$TESTNAME folders with spaces"
-    (does_it_works '/root/boh/a spaced folder/bacon' boh /root/boh 0) $status -eq 0
+    (does_it_works '/root/boh/a spaced folder/bacon' boh /root/boh) $status -eq 0
 end
--- upto.fish
+++ upto.fish
@@ -1,13 +1,12 @@
-function upto -d "Go up to a certain directory"
-    set -l pieces (printf "%s\n" $PWD | tr "/" "\n")
-    
-    if contains -- $argv $pieces
-        if set -l p (contains -- --index $argv $pieces)
-            set -l dest (printf "%s\n" $pieces[1..$p] | tr " " "/")
-            cd "$dest"
-        
-            return
-        end
+function upto -a where -d "Go up to a certain directory"
+    set -l pieces ( pwd | tr "/" "\n")
+
+    if contains -- $where $pieces
+        set -l p (contains --index -- $where $pieces)
+        set -l dest (echo $pieces[1..$p] | tr " " "/")
+        cd "$dest"
+
+        return
     end
 
     return 1
